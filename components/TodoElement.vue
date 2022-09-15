8b3fcf56e521043be6db8ed8ea4c7484d5ce6844
Макс Головин <ma32kc@gmail.com>
2022-09-15 21:04:11 +0300

    Adding github pages deploy workflow

---
+++ components/TodoElement.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TodoElement"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/UI/TodoButton.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TodoButton"
+}
+</script>
+
+<style scoped>
+
+</style>
